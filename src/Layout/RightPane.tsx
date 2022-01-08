897858987faedf7d661aaf471c7078193fd2448f
mutlumehmet <mcrmehmet@gmail.com>
2022-01-09 00:17:15 +0300

    initial commit

---
+++ src/Layout/RightPane.tsx
@@ -0,0 +1,11 @@
+import styles from "./RightPane.module.css";
+
+const RightPane = () => {
+  return (
+    <div className={styles.rightpane}>
+      <h2>Right Pane</h2>
+    </div>
+  );
+};
+
+export default RightPane;
---
+++ src/Layout/SideBar.tsx
@@ -0,0 +1,11 @@
+import styles from "./SideBar.module.css";
+
+const SideBar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <h2>Sidebar</h2>
+    </div>
+  );
+};
+
+export default SideBar;

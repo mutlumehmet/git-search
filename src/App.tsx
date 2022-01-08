d0aa4cc82805a50a692c013cb74193bef2676401
mutlumehmet <mcrmehmet@gmail.com>
2022-01-09 00:20:39 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Layout from "./Layout/Layout";
 
 function App() {
-  return <Layout />;
+  return <div></div>;
 }
 
 export default App;

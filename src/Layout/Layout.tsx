b407eaef7f48d036e2dffdf64ae6482329804e89
mutlumehmet <mcrmehmet@gmail.com>
2022-01-09 01:27:26 +0300

    css arrangements

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,13 +1,18 @@
 import NavBar from "./NavBar";
 import RightPane from "./RightPane";
 import SideBar from "./SideBar";
+import styles from "./Layout.module.css";
 
 const Layout = () => {
   return (
-    <div>
-      <NavBar />
-      <SideBar />
-      <RightPane />
+    <div className={styles.main}>
+      <div className={styles["nav-row"]}>
+        <NavBar />
+      </div>
+      <div className={styles["main-row"]}>
+        <SideBar />
+        <RightPane />
+      </div>
     </div>
   );
 };
